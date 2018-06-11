e290c6a027302a7e84c7a2d3cf49d4639f36463b
Alex Noble and Jimmy McNamara <jimmy@>
2018-06-11 14:31:02 -0700

    finish firebase:deleting entries

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -28,5 +28,10 @@ export class AlbumService {
                                 artist: localUpdatedAlbum.artist,
                                 description: localUpdatedAlbum.description});
   }
+  
+  deleteAlbum(localAlbumToDelete){
+    var albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 
 }
